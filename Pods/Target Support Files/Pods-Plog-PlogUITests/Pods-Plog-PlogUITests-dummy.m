30d2f0a0628051433b067edbfbe8bd93e8c303e5
beige-cz <yellowblue9992@gmail.com>
2022-08-08 15:03:13 +0900

    Add Pods

---
+++ Pods/Target Support Files/Pods-Plog-PlogUITests/Pods-Plog-PlogUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Plog_PlogUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Plog_PlogUITests
+@end
---
+++ Pods/Target Support Files/Pods-Plog/Pods-Plog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Plog : NSObject
+@end
+@implementation PodsDummy_Pods_Plog
+@end
