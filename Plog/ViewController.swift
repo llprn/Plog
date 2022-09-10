f6981140c498bae465183f0b539429a8921543ee
llprn <yihan0203@naver.com>
2022-09-10 23:03:34 +0900

    Fix weather api

--- Plog/ViewController.swift
+++ Plog/ViewController.swift
@@ -10,40 +10,18 @@ import KakaoSDKUser
 
 import FirebaseFirestore
 
-//marker class
-/*class Marker:NSObject,MKAnnotation{
-    let title:String?
-    let coordinate:CLLocationCoordinate2D
-    init(title:String?,
-         coordinate:CLLocationCoordinate2D){
-        self.title = title
-        self.coordinate = coordinate
-        super.init()
-    }
-}*/
-class ViewController: UIViewController, CLLocationManagerDelegate/*,MKMapViewDelegate*/ {
-    
-    
-   
+class ViewController: UIViewController, CLLocationManagerDelegate{
     //db
-/*    var documentIDString: String!
-    var uuid: String = ""
-  */
     let db = Firestore.firestore()
 
     @IBOutlet weak var location: UILabel!
+   
+    @IBOutlet weak var mapView: MKMapView!//지도
+    
     //날씨
     @IBOutlet weak var currentTemp: UILabel!
     @IBOutlet weak var minTemp: UILabel!
     @IBOutlet weak var maxTemp: UILabel!
-    
-    //지도
-    @IBOutlet weak var mapView: MKMapView!
-/*    let mark = Marker(
-        title: "숙명여대",
-        coordinate:CLLocationCoordinate2D(latitude:37.54638593013086,longitude: 126.96369838218818))
-  */
-    
     @IBOutlet weak var weatherImg: UIImageView!
     var weather: Weather?
     var main: Main?
@@ -51,17 +29,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate/*,MKMapViewDel
 
     
     @IBOutlet weak var weatherDescription: UILabel!
-    //   var locationManager = CLLocationManager()
     var locationManager: CLLocationManager!
     var currentLocation: String?
-
-    var longitude: Double!
-    var latitude: Double!
+    var current: CLLocation! //save location
+        
     @IBAction func logout(_ sender: Any) {
         UserApi.shared.logout {(error) in
-            if let error = error {
-                print(error)
-            }
+            if let error = error { print(error) }
             else {
                 print("로그아웃 성공")
                 let newVC = UIStoryboard(name: "Login", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
@@ -80,11 +54,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate/*,MKMapViewDel
         locationManager.delegate = self
         self.locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
         //위도,경도 정보
-        let coor = locationManager.location?.coordinate
-                latitude = coor?.latitude
-                longitude = coor?.longitude
-        
+    /*    let coor = locationManager.location?.coordinate
+        let latitude = coor?.latitude
+        let longitude = coor?.longitude*/
         switch locationManager.authorizationStatus{
             case .denied:
             print("위치 비허용")
@@ -93,122 +67,54 @@ class ViewController: UIViewController, CLLocationManagerDelegate/*,MKMapViewDel
         default:
             break
         }
- //       test.text = ("\(longitude ?? 0)")
-        
-       //map marker 수정필요
- /*       db.collection("startAndEndPoints").document(self.uuid).getDocument { [self] snapshot, error in
-        guard let data = snapshot?.data(), error == nil else {
-                return
-            }
-            print("hi")
-      //      self.startPoint.text = data["startPoint"] as? String
-      //      self.endPoint.text = data["endPoint"] as? String
-                
-         
-            }*/
-      //map marker
-        //weather
-    
-        WeatherService().getWeather{ result in
-                    switch result{
-                    case .success(let weatherResponse): DispatchQueue.main.async {
-                        self.weather = weatherResponse.weather.first
-                        self.main = weatherResponse.main
-                        self.name = weatherResponse.name
-                        self.setWeather()
-                    }
-                    case .failure(_ ):
-                        print("error")
-                        
-                    }
-                   
-                }
-        
-    /*    locationManager = CLLocationManager()
-        locationManager.delegate = self
-        switch locationManager.authorizationStatus {
-        case .denied:
-            print("위치 비허용 상태")
-        case .notDetermined, .restricted:
-            locationManager.requestWhenInUseAuthorization()
-        default:
-            break
-        }*/
- //   }
+
     }
     override func viewWillAppear(_ animated: Bool) {
-            db.collection("startAndEndPoints").getDocuments() { (querySnapshot, err) in
-                if let err = err {
-                    print(err)
-                } else {
-                    for document in querySnapshot!.documents {
-                        // 시작 지점
-                        let startPoint = document.data() ["startPoint"] as! GeoPoint
-                        print(startPoint)
-                        let startAnnotation = MKPointAnnotation()
-                        startAnnotation.coordinate = CLLocationCoordinate2DMake(startPoint.latitude, startPoint.longitude)
-                        self.mapView.addAnnotation(startAnnotation)
-                        
-                        // 종료 지점
-                        let endPoint = document.data() ["endPoint"] as! GeoPoint
-                        print(endPoint)
-                        print("==============")
-                        let endAnnotation = MKPointAnnotation()
-                        endAnnotation.coordinate = CLLocationCoordinate2DMake(endPoint.latitude, endPoint.longitude)
-                        self.mapView.addAnnotation(endAnnotation)
-                    }
+        db.collection("startAndEndPoints").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print(err)
+            } else {
+                for document in querySnapshot!.documents {
+                    // 시작 지점
+                    let startPoint = document.data() ["startPoint"] as! GeoPoint
+                    print(startPoint)
+                    let startAnnotation = MKPointAnnotation()
+                    startAnnotation.coordinate = CLLocationCoordinate2DMake(startPoint.latitude, startPoint.longitude)
+                    self.mapView.addAnnotation(startAnnotation)
+                    // 종료 지점
+                    let endPoint = document.data() ["endPoint"] as! GeoPoint
+                    print(endPoint)
+                    print("==============")
+                    let endAnnotation = MKPointAnnotation()
+                    endAnnotation.coordinate = CLLocationCoordinate2DMake(endPoint.latitude, endPoint.longitude)
+                    self.mapView.addAnnotation(endAnnotation)
                 }
             }
-    //map
- /*   func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            
-            guard !annotation.isKind(of: MKUserLocation.self) else {
-                return nil
-            }
-            
-            let annotationIdentifier = "AnnotationIdentifier"
-            
-            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
-            if annotationView == nil {
-                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
-                annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-                annotationView!.canShowCallout = true
-            }
-            else {
-                annotationView!.annotation = annotation
-            }
-            
-            // resize image
-            let pinImage = UIImage(named: "sprout.png")
-            let size = CGSize(width: 25, height: 25)
-            UIGraphicsBeginImageContext(size)
-            pinImage!.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-            let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-
-            annotationView!.image = resizedImage
-           
-            return annotationView
+        
         }
-}*/
-
-/*주석처리
-class LocationService {
-    static var shared = LocationService()
-    var longitude: Double!
-    var latitude: Double!
-
-}*/
-}
+    
+    }
 }
 extension ViewController{
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
         currentLocation = "\(locValue.latitude),\(locValue.longitude)"
-     /*   if let location = locations.first {
-            print("위치 업데이트!")
-            print("위도 : \(location.coordinate.latitude)")
-            print("경도 : \(location.coordinate.longitude)")
-        }*/
+        let latitude = locValue.latitude
+        let longitude = locValue.longitude
+        print(longitude,latitude)
+        WeatherService().getWeather(lat:latitude,lon:longitude ){ [self] result in
+            switch result{
+                        case .success(let weatherResponse): DispatchQueue.main.async {
+                            self.weather = weatherResponse.weather.first
+                            self.main = weatherResponse.main
+                            self.name = weatherResponse.name
+                            self.setWeather()
+                        }
+                        case .failure(_ ):
+                            print("error")
+                            
+                        }
+                    }
     }
         
     // 위치 가져오기 실패
@@ -216,64 +122,25 @@ extension ViewController{
         print("error")
     }
 
-
- /*   func locationManager(_ manager: CLLocationManager, didUpdatedLocations locations: [CLLocation]){
-        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-        currentLocation = "\(locValue.latitude),\(locValue.longitude)"
-        testt.text = currentLocation
-    }*/
-    
-    //map marker
-  /*  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        guard !annotation.isKind(of: MKUserLocation.self) else {
-                return nil
-        }
-            
-            let annotationIdentifier = "AnnotationIdentifier"
-            
-            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
-            if annotationView == nil {
-                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
-                annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-                annotationView!.canShowCallout = true
-            }
-            else {
-                annotationView!.annotation = annotation
-            }
-            
-            // resize image
-            let pinImage = UIImage(named: "sprout.png")
-            let size = CGSize(width: 25, height: 25)
-            UIGraphicsBeginImageContext(size)
-            pinImage!.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-            let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-
-            annotationView!.image = resizedImage
-           
-            return annotationView
-        }*/
-    
     private func setWeather(){
         let url = URL(string: "https://openweathermap.org/img/wn/\(self.weather?.icon ?? "00")@2x.png")
         let data = try? Data(contentsOf: url!)
-            if let data = data {
-               weatherImg.image = UIImage(data: data)
-           }
+        if let data = data {
+            weatherImg.image = UIImage(data: data)
+        }
         weatherDescription.text = weather?.description
         location.text = name
         currentTemp.text = "\(Int(main!.temp-273))"
         maxTemp.text = "\(Int(main!.temp_max-273))"
         minTemp.text = "\(Int(main!.temp_min-273))"
     }
-
 }
+
 extension ViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
         guard !annotation.isKind(of: MKUserLocation.self) else {
             return nil
         }
-        
         let annotationIdentifier = "AnnotationIdentifier"
         
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
@@ -285,16 +152,13 @@ extension ViewController: MKMapViewDelegate {
         else {
             annotationView!.annotation = annotation
         }
-        
         // resize image
         let pinImage = UIImage(named: "sprout.png")
         let size = CGSize(width: 25, height: 25)
         UIGraphicsBeginImageContext(size)
         pinImage!.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-
         annotationView!.image = resizedImage
-       
         return annotationView
     }
 }
--- Plog/WeatherService.swift
+++ Plog/WeatherService.swift
@@ -10,62 +10,19 @@ enum NetworkError: Error {
     case decodingError
 }
 
+class WeatherService:NSObject, CLLocationManagerDelegate{
+    let uurl: String = "https://api.openweathermap.org/data/2.5/weather?"
+    let aid: String = "&appid=6cb03060feeddc6a874d4e5aabfe15f9"
 
-
-/*class Mark{
-    let coordinate:CLLocationCoordinate2D
-    init(
-         coordinate:CLLocationCoordinate2D){
-        self.coordinate = coordinate
-    }
-}*/
-/*
-class LocationService {
-    static var shared = LocationService()
-    var longitude:Double!
-    var latitude:Double!
-}*/
-class WeatherService {
-    // API 호출을 위한 URL
-    let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=6cb03060feeddc6a874d4e5aabfe15f9")
-  
-//                "Latitude: \(locationCoordinate.latitude),
-  //  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
- /*   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation],completion:@escaping (Result<WeatherResponse, NetworkError>) -> Void) {
-        if let location = locations.first {
-           let latitude = location.coordinate.latitude
-            let longitude = location.coordinate.longitude
-           guard let url = URL(string:
-                                "https://api.openweathermap.org/data/2.5/weather?appid=6cb03060feeddc6a874d4e5aabfe15f9&lat=\(latitude)&lon=\(longitude)") else{
-               return completion(.failure(.badUrl))*/
-        //}
-        //    getWeather(latitude:lat,longitude:lon)
-        //    print(lat,lon)
-     //   }
-       
-         
-       // } locationManager
-    
-   //func getWeather(/*_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation],*/ /*latitude:CLLocationDegrees,longitude:CLLocationDegrees,url,*/completion: @escaping (Result<WeatherResponse, NetworkError>) -> Void) {
-      
+    func getWeather(lat:Double,lon:Double,completion: @escaping (Result<WeatherResponse, NetworkError>) -> Void) {
+        let lati = "lat=\(lat)&"
+        let longi = "lon=\(lon)"
         
-    //    guard let location = locations.first
-   //     else {return}
-//        let latitude = location.coordinate.latitude
-//        let longitude = location.coordinate.longitude
-      //  test.text = ("\(longitude ?? 0)")
-   //     guard let url = "\(wurl)&lat=\(latitude)&lon=\(longitude)" else{
-/*핸드폰으로 돌려보기  "\(details.rate)"
-    let longitude = LocationService.shared.longitude! as? Int
-     let latitude = LocationService.shared.latitude! as? Int
-    let url = URL(string:
-                    "https://api.openweathermap.org/data/2.5/weather?appid=6cb03060feeddc6a874d4e5aabfe15f9&lat=\(LocationService.shared.longitude!)&lon=\(LocationService.shared.latitude!)")*/
- 
-    func getWeather(completion: @escaping (Result<WeatherResponse, NetworkError>) -> Void) {
-        guard let url = url else {
+        guard let url:URL = URL(string:uurl+lati+longi+aid) else{
             return completion(.failure(.badUrl))
         }
        
+        
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil else {
                 return completion(.failure(.noData))
@@ -77,11 +34,14 @@ class WeatherService {
            // 성공
             if let weatherResponse = weatherResponse {
                print(weatherResponse)
-               completion(.success(weatherResponse)) // 성공한 데이터 저장
+               completion(.success(weatherResponse)) //데이터 저장
             } else {
+                print(url)
                completion(.failure(.decodingError))
            }
-       }.resume() // 이 dataTask 시작
+       }.resume()
     }
-   
 }
+
+
+    
